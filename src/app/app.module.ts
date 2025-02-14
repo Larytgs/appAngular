e42107b5f41a0b59c4b5fac32f13a46c2e9336dc
laryTgs <laryssatortato99@gmail.com>
2025-02-14 12:26:02 -0300

    colocado o resumo para fazer um novo projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskListComponent } from './tasks/task-list/task-list.component';
+import { TaskListItemComponent } from './tasks/task-list-item/task-list-item.component';
+import { TaskFormComponent } from './tasks/task-form/task-form.component';
 
 @NgModule({
-  declarations: [ AppComponent],
+  declarations: [ AppComponent, TaskListComponent, TaskListItemComponent, TaskFormComponent],
   imports: [ BrowserModule, AppRoutingModule ],
   providers: [],
   bootstrap: [AppComponent]
