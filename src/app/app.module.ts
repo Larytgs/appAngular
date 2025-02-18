56cd04d757c34b4670a12cecd0a6c053cc6d2e4c
laryTgs <laryssatortato99@gmail.com>
2025-02-18 14:31:12 -0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,7 +10,7 @@ import { TaskFormComponent } from './tasks/task-form/task-form.component';
 
 @NgModule({
   declarations: [ AppComponent, TaskListComponent, TaskListItemComponent, TaskFormComponent],
-  imports: [ BrowserModule, AppRoutingModule ],
+  imports: [ BrowserModule, AppRoutingModule, FormsModule ],
   providers: [],
   bootstrap: [AppComponent]
 })
