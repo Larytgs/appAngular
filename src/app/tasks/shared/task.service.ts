2ed91198dd0960501f60ad44eb63b5f19880a96d
laryTgs <laryssatortato99@gmail.com>
2025-02-18 15:08:19 -0300

    Update task.service.ts

--- src/app/tasks/shared/task.service.ts
+++ src/app/tasks/shared/task.service.ts
@@ -25,6 +25,34 @@ export class TaskService {
   getAll() {
     return this.tasks;
   }
+
+  getByID(id: number) { //ele vai no array de tarefas
+    const task = this.tasks.find((value) => value.id == id);
+    return task; //procurar(metodo find) a primeria tarefa q ele achar que tem esse id, é oq ele vai devolver para tela
+  }
+
+  save(task: Task) {
+    if (task.id) { //verifar se tem id, se tiver entao ele entende q é uma edição
+      const taskArr = this.getByID(task.id); //recupera a tarefa pelo ID
+      if (taskArr) { //verifica se a tarefa foi encontrada
+        taskArr.description = task.description; //vai atualizar a descrição
+        taskArr.completed = task.completed; //se ele ta completa ou nao
+      }
+    }
+     else { //caso nao tenha id, ele entende q é uma conclusao
+      const lastId = this.tasks[this.tasks.length-1].id; //ele vai gerar um id, indo na ultima tarefa cadastrada
+      task.id = lastId + 1; //gera um novo ID
+      task.completed = false; //marca como não completada
+      this.tasks.push(task); //adiciona a nova tarefa ao array
+    }
+  }
+
+  delete(id: number) {
+    const taskIndex = this.tasks.findIndex((value) => value.id == id); //recuperando o index da tarefa
+    this.tasks.splice(taskIndex, 1); //chamando o metodo splice, para remover
+  }
+
+
 }
 
 
