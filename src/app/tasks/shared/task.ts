4e1a8f7564615cee8fe7d46176a94ed88a322343
laryTgs <laryssatortato99@gmail.com>
2025-02-18 11:26:42 -0300

    Update task.ts

--- src/app/tasks/shared/task.ts
+++ src/app/tasks/shared/task.ts
@@ -2,4 +2,12 @@ export class Task {
   id: number;
   description: string;
   completed: boolean;
+
+  constructor(id: number, description: string, completed: boolean) {
+    this.id = id;
+    this.description = description;
+    this.completed = completed;
+  }
 }
+//Tem que adicionar os valores padrão às propriedades na declaração
+//Caso queira que os valores sejam passados ​​ao criar um objeto, use um construtor
