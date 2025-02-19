de9512ea896e1d34dd48a1d45360e3df2c75be11
laryTgs <laryssatortato99@gmail.com>
2025-02-19 16:51:28 -0300

    save, delete, change

--- src/app/tasks/task-list-item/task-list-item.component.ts
+++ src/app/tasks/task-list-item/task-list-item.component.ts
@@ -1,5 +1,6 @@
+import { TaskService } from './../shared/task.service';
+import { Task } from './../shared/task';
 import { Component, OnInit, Input } from '@angular/core';
-import { Task } from '../shared/task';
 
 @Component({
   selector: 'app-task-list-item',
@@ -10,9 +11,18 @@ export class TaskListItemComponent implements OnInit {
   @Input() //atritubo do meu componente, onde vou passar por parametro, para esse componente qual é a task, e vou conseguir reenderizar na tela
   task!: Task;
 
-  constructor() {}
+  constructor(private taskService: TaskService) {}
+  //importar ele aqui
 
   ngOnInit(){
 
+  }
+
+  remove(task: Task) {
+    this.taskService.delete(task.id); //ja tem o metodo de excluir, no 'task.service.ts'... importar no constructor em cima
+  }
+
+  onCompletedCheckChange(task: Task){
+    this.taskService.save(task); //ja tem o metodo de salvar, no 'task.service.ts'
   }
 }
